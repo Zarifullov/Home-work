df47b820ba092bc6d1ba76c111b3db49dc38ddf8
Renat <rzarifullov@gmail.com>
2022-07-24 18:26:25 +0300

    Task3 added

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,20 +1,15 @@
 ﻿int x = 0;
-int y = 0;
-int z = 0;
 
 Console.WriteLine("Введите число x: ");
 x = int.Parse(Console.ReadLine());
 
-y = x/2;
-string y = Console.ReadLine();
 
-bool y = int.TryParse(y, out z);
 
-if (y)
+if (x%2 == 0)
 {
     Console.Write($"{x} - чётное число");
 }
-if (!y)
+else
 {
    Console.Write($"{x} - нечётное число");  
 }
