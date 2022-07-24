692fb241c9d5837b19906b2d729e42dee5a05e2c
Renat <rzarifullov@gmail.com>
2022-07-24 17:14:09 +0300

    Task2 was added

---
+++ Task 2/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 0;
+int b = 0;
+int c = 0;
+
+int max = a;
+
+Console.WriteLine("Введите число a: ");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число c: ");
+c = int.Parse(Console.ReadLine());
+
+if(b > max) max = b;
+if(c > max) max = c;
+{
+  Console.Write($"Максимальное число: {max}");  
+}
+
