4f0af5df29d2e630dbf07dbb4c8949e4b942d204
Renat <rzarifullov@gmail.com>
2022-07-24 16:49:41 +0300

    Task1

---
+++ Task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 0;
+int b = 0;
+
+
+Console.WriteLine("Введите число a: ");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+b = int.Parse(Console.ReadLine());
+
+if(a < b)
+{
+  Console.Write($"max = {b}, min = {a}");  
+}
+
+else if(b < a)
+{
+  Console.Write($"max = {a}, min = {b}");  
+}
+else
+{
+Console.Write("Оба числа одинаковые");
+}
+
