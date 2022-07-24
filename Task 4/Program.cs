c66410bc61707b6dd321b4ab43acd5d84a8b1b27
Renat <rzarifullov@gmail.com>
2022-07-24 21:56:24 +0300

    Task4 added

---
+++ Task 4/Program.cs
@@ -0,0 +1,15 @@
+﻿int x = 0;
+
+Console.WriteLine("Введите число x: ");
+x = int.Parse(Console.ReadLine());
+
+
+
+if (x%2 == 0)
+{
+    Console.Write($"{x} - чётное число");
+}
+else
+{
+   Console.Write($"{x} - нечётное число");  
+}
